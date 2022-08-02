c2fc0018a49655960cc1e194b70fb7c4323009e4
Vlad Vyalov <vyalov92@yandex.ru>
2022-08-03 01:17:32 +0500

    main

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,7 +15,7 @@ const getCards = (req, res) => {
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send(card))
+    .then((card) => res.status(200).send(card._id))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
@@ -74,7 +74,7 @@ const dislikeCard = (req, res) => {
       if (card) {
         res.send(card);
       }
-      return res.status(200).send({ message: 'Карточка с указанным _id не найдена' });
+      return res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
