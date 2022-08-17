80c83c219c8dfd08b4530cc2021cb8168b11f556
Vlad Vyalov <vyalov92@yandex.ru>
2022-08-17 15:41:54 +0500

    main

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const RequestError = require('../errors/request-error');
 const EmailError = require('../errors/email-error');
