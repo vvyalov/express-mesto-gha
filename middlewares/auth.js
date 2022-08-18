538693450090f4f56f31d60ab4d5333ba898feb0
Vlad Vyalov <vyalov92@yandex.ru>
2022-08-18 23:25:44 +0500

    main

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,10 +2,11 @@ const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-error');
 
 module.exports = (req, res, next) => {
-  const authorization = req.cookie;
+  const authorization = req.cookies;
 
   if (!authorization) {
-    throw new AuthError('Необходима авторизация');
+    next(new AuthError('Необходима авторизация'));
+    return;
   }
 
   const token = authorization.jwt;
@@ -14,7 +15,8 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, '62e90cd9d7cbfdc9705395ce');
   } catch (err) {
-    throw new AuthError('Необходима авторизация');
+    next(new AuthError('Необходима авторизация'));
+    return;
   }
 
   req.user = payload;
